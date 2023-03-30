a369c3a8fdfaa25c1f84785d9e7bc548ee251419
lirummirul <89711820+lirummirul@users.noreply.github.com>
2023-03-30 19:37:28 +0300

    init proj

---
+++ Meditation/Meditation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Лада on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MVP используем, фабричный паттерн желательно использовать (refactoring guru)
+    // Разобраться мне нужно наконец-то с weak
+    // Completion 
+}
+
